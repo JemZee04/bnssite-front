d7d74f3a7b8c7b4e1e4fe26465f4978b9fe81439
katya <katya_yuneva@mail.ru>
2024-06-23 19:44:48 +0400

    stylized favorites

--- src/components/productInFavorites/ProductInFavorites.tsx
+++ src/components/productInFavorites/ProductInFavorites.tsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { ProductInFavorites } from "../../store/slices/favorites";
 import { PRODUCT_PATH } from "../../shared/utils/constants";
 import { AddToFavoritesButton } from "../AddToFavoritesButton";
+import styles from "./ProductInFavorites.module.css";
 
 type ProductCartItemProps = {
     item: ProductInFavorites
@@ -15,8 +16,9 @@ export const ProductFavoriteItemCard: React.FC<ProductCartItemProps> = ({ item }
 
     return (
         <Row gutter={[10, 0]}>
-            <Col span={12} style={{ padding: 10 }}>
+            <Col span={11} style={{ padding: 10 }}>
                 <Image
+                    style={{width: 268, height: 296, cursor: "pointer"}}
                     onClick={() => navigate(`${PRODUCT_PATH}/${item.id}`)}
                     preview={false}
                     alt={item.images?.at(0)?.alt ?? 'Фото товара'}
@@ -28,18 +30,22 @@ export const ProductFavoriteItemCard: React.FC<ProductCartItemProps> = ({ item }
                 {/* <Space direction='vertical' size={10}> */}
                 <Row>
                     <Col flex='auto'>
-                        <h3>{item.name}</h3>
+                        <h3 style={{fontFamily: "Source Sans 3", fontWeight: 700, fontSize: 24, color: "#FFFFFF"}}>{item.name}</h3>
                     </Col>
                     <Col span={2}>
                         <AddToFavoritesButton item={item} />
                     </Col>
                 </Row>
                 {/* TODO вставить ссылку на бренд */}
-                <Link to={''} >
+                <Link to={''} style={{fontFamily: "Source Sans 3", fontWeight: 400, fontSize: 16,color: "#FFFFFF"}}>
                     {item.shop?.name}
                 </Link>
-                <div>{item.sizes?.at(0)?.name}</div>
-                <div>{item.price}</div>
+                <div className={styles.WrapContent}>
+                    <p className={styles.Content}>{item.sizes?.at(0)?.name ?? "Размер не выбран"}</p>
+                    <div className={styles.Separator}></div>
+                    <p className={styles.Content}>{item.colors?.at(0)?.name ?? "Цвет не выбран"}</p>
+                </div>
+                <p className={styles.Content} style={{fontWeight: 700, fontSize: "24px"}}>{`${item.price} ₽`}</p>
             </Col>
         </Row>
     );
